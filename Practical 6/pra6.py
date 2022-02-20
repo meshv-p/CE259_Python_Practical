6bcfdc464726a18c8322373510e56c90c52dddb6
meshv-p <77038149+meshv-p@users.noreply.github.com>
2022-02-20 21:45:40 +0530

    Done Prac 6 20CE092

---
+++ Practical 6/pra6.py
@@ -0,0 +1,26 @@
+'''
+    Name: Meshv U Patel
+    Id: 20CE092
+    Link: https://github.com/meshv-p/CE259_Python_Practical
+    Aim: You are given n words. Some words may repeat. For each word, output its number of occurrences. The output order should correspond with the input order of appearance of the word. See the sample input/output for clarification. 
+'''
+# find the number of occurences of each word
+'''input: 4 
+        bcdef 
+        abcdefg 
+        bcde 
+        bcdef 
+    output:3 
+            2 1 1 
+'''
+
+
+
+
+
+   
+
+
+
+
+
