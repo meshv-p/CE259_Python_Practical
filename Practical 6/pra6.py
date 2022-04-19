53b927f5cfb98fc622af74e80c0544329909101b
Meshv Patel <77038149+meshv-p@users.noreply.github.com>
2022-04-19 19:05:59 +0530

    Update code

--- Practical 6/pra6.py
+++ Practical 6/pra6.py
@@ -13,7 +13,25 @@
     output:3 
             2 1 1 
 '''
+a = int(input())
+arr = []
+for i in range(a):
+    b = input()
+    arr.append(b)
+set1 = set(arr)
+print(len(set1))
 
+arr2 = []
+arr3 = []
+for i in arr:
+    if i in arr2:
+        pass
+    else:
+        arr2.append(i)
+        arr3.append(arr.count(i))
+
+for j in arr3:
+    print(j, end=' ')
 
 
 
